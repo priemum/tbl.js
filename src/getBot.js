a8698981834159e0e7dc0e7ada117cef6bcddb10
EdwardAsaad <57170855+EdwardAsaad@users.noreply.github.com>
2019-11-02 19:16:00 +0200

    Create getBot.js

---
+++ src/getBot.js
@@ -0,0 +1,5 @@
+async function getBot (ID) {
+  if (!ID) return 'error no id';
+  else return JSON.parse (await require ('request-promise') ('https://toxic-api.glitch.me/api/bots/' + ID));
+}
+module.exports = getBot;
